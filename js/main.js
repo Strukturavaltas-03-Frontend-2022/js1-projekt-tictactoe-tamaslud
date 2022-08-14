85203221dc947bd565aad7904189542d0b1ccec1
tamaslud <tamaslud@yahoo.com>
2022-08-14 23:08:53 +0200

    Hát ennyi, megint kifutottam az időből a stílussal nem foglalkoztam. a függvényeket nem szerveztem ki a removeeventlistener sehogyse jött össze a győztest csak a konzolra iratom ki.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,29 @@
 let cells = document.querySelectorAll(".cell");
 
 const marksDefault = "-xoxoxoxox";
-let marks = marksDefault.split('');
+let marks = marksDefault.split("");
+let boardArray = Array(9);
+const clusters = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function initBoard() {
+  console.log("board initialization");
+  marks = marksDefault.split("");
+  document.querySelector(".nextPlayer").innerHTML = `${marks.slice(-1)} player's turn`;
+
+  boardArray = Array(9);
+  for (let i = 0; i < cells.length; i += 1) {
+    cells[i].innerHTML = "";
+  }
+}
 
 
 for (let i = 0; i < cells.length; i += 1) {
@@ -10,39 +32,43 @@ for (let i = 0; i < cells.length; i += 1) {
     if (cells[i].innerHTML.length < 1) {
       let nextMark = marks.pop();
       cells[i].innerHTML = nextMark;
-      document.querySelector(".nextPlayer").innerHTML = marks.slice(-1);
-      checkWinner();
-      checkDrawn();
+      boardArray[i] = nextMark;
+      document.querySelector(".nextPlayer").innerHTML = `${marks.slice(-1)} player's turn`;
+      console.log(checkWinner())
     }
-  });
+  }
+  );
 }
 
 document.querySelector(".newgameBtn").addEventListener("click", function () {
   initBoard();
 });
 
-function initBoard() {
-  console.log("board initialization");
-  marks = marksDefault.split('');
-  document.querySelector(".nextPlayer").innerHTML = marks.slice(-1);
-  for (let i = 0; i < cells.length; i += 1) {
-    (cells[i].innerHTML = '');
-    };
-
-}
 
 function checkWinner() {
+  let result = "";
   console.log("checking win situation");
-}
+  for (const cluster of clusters) {
+    let triplet = [];
+    for (const i of cluster) {
+      triplet.push(boardArray[i]);
+    }
+    if (triplet.every((i) => i === "o")) {
+      result = "Player 'o' won the game!";
+      document.querySelector(".nextPlayer").innerHTML = `${result}`;
+     
+    }
+    if (triplet.every((i) => i === "x")) {
+      result = "Player 'x' won the game!";
+      document.querySelector(".nextPlayer").innerHTML = `${result}`;
+    }
+    if (marks.length == 1) {
+      result = "Drawn game!";
+      document.querySelector(".nextPlayer").innerHTML = `${result}`;
+    }
+    
 
-function checkDrawn() {
-  console.log("checking drawn situation");
-}
+  }
 
-/*
-for (let i = 0; i < cells.length; i += 1) {
-  cells[i].addEventListener("click", function () {
-    console.log("cella: " + i + ", tartalma: " + cells[i].innerHTML);
-  });
+  return result;
 }
-*/
